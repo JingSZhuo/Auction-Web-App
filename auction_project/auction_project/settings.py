f003959eb533d8fb9546df3a117a6e84119d171c
ECS522U-GUI-PROJECT-LAB2-GROUP8 <ecs522uguilab2group8@gmail.com>
2022-11-28 17:39:17 +0000

    added corsheaders

--- auction_project/auction_project/settings.py
+++ auction_project/auction_project/settings.py
@@ -38,16 +38,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",     #Allow Vue Application
+]
+
+CSRF_TRUSTED_ORIGINS = [
+     "http://127.0.0.1:5173", 
 ]
 
 ROOT_URLCONF = 'auction_project.urls'
