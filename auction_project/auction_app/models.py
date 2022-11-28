0e643503d5fe7fe85afcc89e864eb8aa42bcbb8e
Miguel Divo <migueldivo11@gmail.com>
2022-11-28 17:28:16 +0000

    creating user model

--- auction_project/auction_app/models.py
+++ auction_project/auction_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_email = models.EmailField(max_length=254)
+    user_dob = models.DateField()
+
+    # to use ImageField you will need to install Pillow
+    #pip install Pillow
+    
+    user_profilePicture = models.ImageField()
+    def __str__(self):
+        return self.user_email
+    def __str__(self):
+        return self.user_dob
+    def __str__(self):
+        return self.user_profilePicture
+
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'user_email': self.user_email,
+            'user_dob': self.user_dob,
+            'user_profilePicture': self.user_profilePicture
+        }
