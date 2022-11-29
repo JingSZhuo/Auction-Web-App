2a6faa83f158d2392a1fb0b3b20b215158992251
kalina-ivanova28 <78103096+kalina-ivanova28@users.noreply.github.com>
2022-11-29 22:55:50 +0000

    views.py

--- auction_project/auction_app/views.py
+++ auction_project/auction_app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpRequest
+from .models import Item,User
+
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Auction app in development...")
+    item_list= Item.objects.order_by('item_title')
+    output = ', '.join((i.item_title for i in item_list))
+    return HttpResponse(output)
+
+@csrf_exempt
