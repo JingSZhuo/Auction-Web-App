fcb81c8635a2f33c2a53cd086eb423e4d51e33c4
kalina-ivanova28 <78103096+kalina-ivanova28@users.noreply.github.com>
2022-11-29 22:15:58 +0000

    Page Design

---
+++ auction_project/auction_app_vue_frontend/src/AddItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Add An Item</h1>
+      <label>Title:</label><br>
+      <input type="text"><br>
+      <label>Description:</label><br>
+      <input type="textarea"><br>
+      <label>Starting Price:</label><br>
+      <input type="number"><br>
+      <label>Picture:</label><br>
+      <input type="text"><br>
+      <label>Date and Time of Auction Finish</label><br>
+      <input type="datetime-local"><br>
+      <button>Add New Item</button>
+    </div>
+  </template>
