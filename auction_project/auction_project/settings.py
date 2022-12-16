b040cf05b3b63622522bcc86f8d322faf63c3306
winnieyap <winnieyap02@hotmail.com>
2022-12-16 05:01:10 +0000

    styling..

--- auction_project/auction_project/settings.py
+++ auction_project/auction_project/settings.py
@@ -118,11 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'auction_app.CustomUser'
 
-#MEDIA 
-
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
